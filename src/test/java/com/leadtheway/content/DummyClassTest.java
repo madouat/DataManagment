abae13868fd7cabaf647f8f15971fc461129a77f
Mamadou Ouattara <mamadou.ouattara@ticketmaster.com>
2016-05-10 13:28:26 -0400

    added test

---
+++ src/test/java/com/leadtheway/content/DummyClassTest.java
@@ -0,0 +1,13 @@
+package com.leadtheway.content;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class DummyClassTest {
+	@Test
+	public void testA() {
+		assertTrue(true);
+	}
+
+}
